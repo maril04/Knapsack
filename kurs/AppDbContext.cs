f0f3b955ca6e7fd348717c823700e849a0e05e1b
maril <maril@MARIVI>
2024-11-26 19:33:51 +0300

    Добавьте файлы проекта.

---
+++ kurs/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dbo
+{
+    // Определение модели данных
+    public class backpack_problem
+    {
+        public int Id { get; set; }
+
+        public string Task_type { get; set; }
+
+        public int Backpack_weight { get; set; }
+        public int Number_of_items { get; set; }
+        public int Answer { get; set; }
+        public string Items { get; set; }
+        public DateTime Date_time { get; set; }
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        // DbSet представляет собой список сущностей, хранящихся в базе данных
+        public DbSet<backpack_problem> backpack_problem { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            // Создание БД, если она отсутствует
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ kurs/Solving_bp_problem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kurs.Database
+{
+    [Table("backpack_problem", Schema = "dbo")] 
+    public class BackpackProblem
+    {
+        public int Id { get; set; }
+        public string Task_type { get; set; }
+        public int Backpack_weight { get; set; }
+        public int Number_of_items { get; set; }
+        public int Answer { get; set; }
+        public string Items { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy hh:mm:ss}",
+       ApplyFormatInEditMode = true)]
+        public DateTime Date_time { get; set; }
+    }
+    public class AppDbContext : DbContext
+    {
+        public DbSet<BackpackProblem> BackpackProblems { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Указываем связь модели с таблицей
+            modelBuilder.Entity<BackpackProblem>()
+                .ToTable("backpack_problem", "dbo");
+        }
+    }
+}
